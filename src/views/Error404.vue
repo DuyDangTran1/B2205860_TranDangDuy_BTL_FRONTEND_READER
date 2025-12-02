f8bc455e204647b33d9bfb6d1b3bdd4e7402d897
DangDuyTran1 <tuechst1@gmail.com>
2025-12-02 18:29:04 +0700

    Trang loi 404

---
+++ src/views/Error404.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container text-center">
+    <RouterLink to="/">
+      <i class="fa-solid fa-home"></i>
+      <p>Sai đường dẫn rồi quay lại trang chủ thôi!</p>
+    </RouterLink>
+  </div>
+</template>
+<style lang="css" scoped>
+i {
+  margin-top: 120px;
+  font-size: 200px;
+  color: blue;
+}
+</style>
